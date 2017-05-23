c7b0dfff0515d00ad9e3038475f4b97e483dbf8b
chutipong <chutipong@fungjai.com>
2017-05-23 09:52:06 +0700

    update marker & polyline-point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  title = 'Try Augular Google Map!';
+  latA:number  = 13.7893182;
+  lngA:number = 100.5789861;
+  zoom:number = 17;
+
+  mapMarkers = [];
+  mapPointer = [];
+
+  showLine:boolean = false;
+
+  constructor() {}
+
+  randomUpdateMarker = () => {
+    this.latA += 0.00005
+    this.lngA += 0.00005
+    this.mapMarkers[0] = {lat: this.latA, long: 100.5789861};
+    this.mapMarkers[1] = {lat: 13.7893182, long: this.lngA};
+
+    this.mapPointer[1] = {lat: this.latA, long: 100.5789861};
+    this.mapPointer[3] = {lat: 13.7893182, long: this.lngA};
+  }
+
+  ngOnInit() {
+    this.mapMarkers.push({lat: 13.7893182, long: 100.5789861});
+    this.mapMarkers.push({lat: 13.7893182, long: 100.5789861});
+
+    this.mapPointer.push({lat: 13.7893182, long: 100.5789861});
+    this.mapPointer.push({lat: 13.7893182, long: 100.5789861});
+    this.mapPointer.push({lat: 13.7893182, long: 100.5789861});
+    this.mapPointer.push({lat: 13.7893182, long: 100.5789861});
+
+    this.showLine = true;
+    setInterval(() => {
+      this.randomUpdateMarker();
+    }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +13,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAvH2rNCcpCxpnvTbOeLH3fJwcz40q6CKg'
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:  [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
